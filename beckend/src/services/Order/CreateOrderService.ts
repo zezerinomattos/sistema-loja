c32e5e772d770967f50abd83aefbecbcd1c64897
zezerinodev <mattoszz@hotmail.com>
2023-06-28 17:21:45 -0300

    Ajuste na rota de criar order.

--- beckend/src/services/Order/CreateOrderService.ts
+++ beckend/src/services/Order/CreateOrderService.ts
@@ -11,6 +11,27 @@ interface OrederRequest{
 class CreateOrderService{
     async execute({ status, draft, colaborado, cliente, caixa_id }: OrederRequest){
 
+        const caixaAberto = await prismaClient.caixa.findFirst({
+            where: {
+                id: caixa_id
+            },
+            select:{
+                status: true,
+            },
+        });
+
+        if(!caixa_id){
+            throw new Error('Informe um caixa');
+        }
+
+        if(!caixaAberto){
+            throw new Error('Caixa inválido')
+        }
+
+        if(caixaAberto.status === false){
+            throw new Error('Caixa fechado')
+        }
+
         const order = await prismaClient.order.create({
             data: {
                 status: status,
