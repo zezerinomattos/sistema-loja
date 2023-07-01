c3ee0a1ac85bd9946873ade8343f25af981f8179
zezerinodev <mattoszz@hotmail.com>
2023-07-01 13:50:32 -0300

    Rota para cancelar um registro de caixa e atualizar o valor do saldo finalizado.

--- beckend/src/services/Caixa/CancelarRegistroCaixaService.ts
+++ beckend/src/services/Caixa/CancelarRegistroCaixaService.ts
@@ -6,6 +6,11 @@ interface RegistroCaixaRequest{
 
 class CancelarRegistroCaixaService{
     async execute({ registro_caixa_id }: RegistroCaixaRequest){
+
+        if(!registro_caixa_id){
+            throw new Error('Informe um ID de Registro de caixa');
+        }
+
         const registroCaixa = await prismaClient.registroCaixa.findUnique({
             where: {id: registro_caixa_id},
             include: {
@@ -14,18 +19,31 @@ class CancelarRegistroCaixaService{
                     select: {
                         saldo: true
                     }
-                }
+                },
             },
         });
 
-        //Retirando o valor do registro para o saldo de caixa
-        const valorSaldo = registroCaixa.valor_recebido - registroCaixa.troco;
+        if(!registroCaixa){
+            throw new Error('Registro de caixa não encontrado');
+        }
+
+        // Retirando o valor do registro para o saldo de caixa
+        let valorSaldoDecremento = 0;
+
+        if (registroCaixa.forma_pagamento === 'CARTÃO') {
+            const valorSaldoCartao = registroCaixa.valor_recebido - (registroCaixa.entradacartao?.valor_entrada ?? 0) - registroCaixa.troco;
+
+            valorSaldoDecremento = valorSaldoCartao;
+
+        } else {
+            valorSaldoDecremento = registroCaixa.valor_recebido - registroCaixa.troco;
+        }
 
         const caixa = await prismaClient.caixa.update({
             where: {id: registroCaixa.caixa_id},
             data:{
                 saldo: {
-                    decrement: valorSaldo,
+                    decrement: valorSaldoDecremento,
                 }
             }
         });
