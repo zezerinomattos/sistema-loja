5be5a4ff37ab3aaeddbfeb47c74737e645f77447
zezerinodev <mattoszz@hotmail.com>
2023-07-06 20:02:21 -0300

    Projeto front-end iniciado, limpo e pronto para a construção.

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.scss';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hollo</h1>
+    </>
+  )
+}
